3d314210f36145aa7e8862ec126dab5e124ed2dd
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-02-27 17:35:13 -0600

    Add string formatting code for practice

---
+++ src/strings.js
@@ -0,0 +1,19 @@
+const fullName = "Christopher Stokley";
+
+const courseNum = "CIS 177";
+
+const expectedGrade = 95;
+
+const string =
+	fullName +
+	" is taking " +
+	courseNum +
+	" and expects to earn " +
+	expectedGrade +
+	"%";
+
+const literal = `${fullName} is taking ${courseNum} and expects to earn ${expectedGrade}%`;
+
+console.log(string);
+
+console.log(literal);
