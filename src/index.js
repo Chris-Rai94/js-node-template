4453d74b0d8e85a4c67b9bc3219baf94f39f7a7c
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-02-27 17:49:49 -0600

    Add method displayInfo for practice

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const student = {
+	name: "Sarah",
+	course: "JavaScript",
+	grade: 95,
+
+	displayInfo() {
+		return `${this.name} is taking ${this.course} and expects to earn ${this.grade}.`;
+	},
+};
