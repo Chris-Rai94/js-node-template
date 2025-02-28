3c14c2456d1f3879dcba764f6c9252b78f56c7fc
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-02-27 21:55:28 -0600

    Combine Strings

--- src/functions.js
+++ src/functions.js
@@ -13,3 +13,8 @@ function formatUsername(firstName, lastName) {
 	return `${lastName}, ${firstName}`;
 }
 console.log(formatUsername("John", "Doe"));
+//test 3
+function combineStrings(str1, str2) {
+	return `${str1} ${str2}`;
+}
+console.log(combineStrings("Hello", "World"));
