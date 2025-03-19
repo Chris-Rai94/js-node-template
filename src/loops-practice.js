03ef071098a63247cf1a43e0d7b5b368bdb524f7
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-03-19 08:25:45 -0500

    Add comments explaining difference between the for and while loop

--- src/loops-practice.js
+++ src/loops-practice.js
@@ -1,10 +1,11 @@
-/*for (i = 1; i < 11; i++) {
+for (let i = 1; i < 11; i++) {
 	// Takes i and counts to 10 starting from 1
 	console.log(i);
-}*/
+}
 
-i = 1;
+i = 1; // Takes i and counts to 10 starting from 1
 while (i < 11) {
 	console.log(i);
 	i++;
 }
+/* The for loop has i declared in the loop, while the while loop has i declared outside of the loop. Both loops will count to 10 starting from 1. */
