7ac752503e32751f4dee59a5eb8d9aa19abd4d99
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-03-19 08:30:02 -0500

    Add array of favorite games

--- src/loops-practice.js
+++ src/loops-practice.js
@@ -9,3 +9,12 @@ while (i < 11) {
 	i++;
 }
 /* The for loop has i declared in the loop, while the while loop has i declared outside of the loop. Both loops will count to 10 starting from 1. */
+
+const favGames = [// Array of favorite games
+    "Monochrome Mobius: Rights and Wrongs Forgotten", 
+    "The Legend of Zelda Twilight Princess",
+    "Monster Hunter Wilds",
+    "Sonic Adventure 2 Battle",
+    "Pathfinder Wrath of the Righteous"
+]
+
